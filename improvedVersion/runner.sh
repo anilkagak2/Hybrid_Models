7aa89a00355ac7746d8600ad3303575e4a634ecf
Anil Kag <anilkagak2@gmail.com>
2023-04-02 08:21:29 -0400

    able to perform one complete run

--- improvedVersion/runner.sh
+++ improvedVersion/runner.sh
@@ -12,7 +12,7 @@ teacher='mobilenetv3_large_100'
 disk_router='DiSK_Router'
 hybrid_router='Hybrid_Router'
 
-epochs=10
+epochs=100
 batch_size=512
 ckpt='./output/train/20230331-192414-tf_mobilenetv3_small_100-224/last.pth.tar'
 
@@ -20,10 +20,11 @@ CUDA_VISIBLE_DEVICES='0,1,2,3' ./distributed_train.sh 4 $data -b $batch_size \
 	--model $student --global_model $teacher -j 16 \
         --disk_router $disk_router --hybrid_router $hybrid_router \
         --opt adamw  --warmup-lr 1e-6 \
-	--sched cosine --epochs $epochs --lr 1e-4 --amp --weight-decay 5e-4 \
+	--sched cosine --epochs $epochs --lr 5e-4 --amp --weight-decay 5e-4 \
 	--model-ema --model-ema-decay 0.9999 --aa rand-m9-mstd0.5-inc1 \
         --pretrained #--resume $ckpt #--torchcompile
 
+#	--sched cosine --epochs $epochs --lr 1e-4 --amp --weight-decay 5e-4 \
 # torchcompile is supported only on GPU with capabilities >= 7.0
 
 #	--model $student --teacher $teacher --routing $routing -j 16 \
